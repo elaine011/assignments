6458449f1837944fc5d0eb4656e7499fb26a84ef
Elaine <e38372779@gmail.com>
2022-07-28 17:59:37 +0800

    update part2

--- part 2/Assignment3.js
+++ part 2/Assignment3.js
@@ -1,29 +1,29 @@
 /** Assignment 3 : Function, Array, and Object
  * calculate the total price of all products after applying a discount */
 
-function calculate(data){
-    let totalSum=0;
-    for(let i=0;i<data.products.length;i++){
-        totalSum += data.products[i]['price']-(data.products[i]['price']*data.discount);
-    }
-    return totalSum;
+function calculate(data) {
+  let totalSum = 0;
+  for (let i = 0; i < data.products.length; i++) {
+    totalSum += data.products[i].price - data.products[i].price * data.discount;
+  }
+  return totalSum;
 }
 
 const discountedPrice = calculate({
-    discount: 0.1,
-    products: [
-        {
-            name: 'Product 1',
-            price: 100
-        },
-        {
-            name: 'Product 2',
-            price: 700
-        },
-        {
-            name: 'Product 3',
-            price: 250
-        }
-    ]
+  discount: 0.1,
+  products: [
+    {
+      name: "Product 1",
+      price: 100,
+    },
+    {
+      name: "Product 2",
+      price: 700,
+    },
+    {
+      name: "Product 3",
+      price: 250,
+    },
+  ],
 });
 console.log(discountedPrice);
--- part 2/Assignment5.js
+++ part 2/Assignment5.js
@@ -1,12 +1,12 @@
 /** Assignment 5 : Callback Function
  *  show delayed result in the console */
 
- function delayedResult(n1, n2, delayTime, callback){
-    setTimeout(callback, delayTime, n1+n2);
+function delayedResult(n1, n2, delayTime, callback) {
+  setTimeout(callback, delayTime, n1 + n2);
 }
-delayedResult(4, 5, 3000, function(result){
-    console.log(result);
+delayedResult(4, 5, 3000, function (result) {
+  console.log(result);
 });
-delayedResult(-5, 10, 2000, function(result){
-    window.alert(result);
+delayedResult(-5, 10, 2000, function (result) {
+  window.alert(result);
 });
