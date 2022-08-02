094aa3f33045c316654c041911e0194000accd76
Elaine <e38372779@gmail.com>
2022-08-02 18:18:34 +0800

    update advanced assignments part 3

--- part 3/Advanced.js
+++ part 3/Advanced.js
@@ -1,6 +1,6 @@
 const APIdata = document.querySelector(".APIdata");
 const moreData = document.querySelector(".moreData");
-let countPages = 5;
+let countPages = 1;
 
 function ajax(url) {
   return fetch(url).then((res) => res.json());
@@ -27,12 +27,12 @@ function render(data) {
     .join("");
 
   APIdata.innerHTML = html;
-
   const topicsHTML = document.querySelector(".topics");
 
   const htmlTopics = data
     .map((topic) => {
       let t = "";
+
       for (let i = 0; i < topic.topics.length; i++) {
         t += `<p >${topic.topics[i]}</p>`;
         console.log(topic.topics[i]);
@@ -40,74 +40,44 @@ function render(data) {
       return t;
     })
     .join("");
-
   topicsHTML.innerHTML = htmlTopics;
+}
 
-  // console.log(htmlTopics);
+function ren(data) {
+  console.log(data);
 
-  // console.log(topicsHTML);
+  const newData = data
+    .map((item) => {
+      let result = `
+    <div class="APIitems">
+      <div class="APIitem">
+      <div class="title">
+        <h2 class="name">${item.name}</h2>
+        <span class="visibility">${item.visibility}</span>
+      </div>
+        <p class="description">${item.description}</p>
+        <div class="topics">`;
+      let b = "";
+      for (let a = 0; a < item.topics.length; a++) {
+        b += `<p>${item.topics[a]}</p>`;
+      }
 
-  // for (let i = 0; i < data.topics.length; i++) {
-  //   const topic = `<p class="topics">${data.topics[i]}</p>`;
-  // }
+      result = result + b + "</div></div></div>";
+      return result;
+    })
+    .join("");
 
-  // data.map((item) => {
-  //   if (item.topics !== []) {
-  //     console.log(item.topics);
-  //     for (let i = 0; i < item.topics.length; i++) {
-  //       const html = `
-  //         <div class="APIitems">
-  //           <div class="APIitem">
-  //             <div class="title">
-  //               <h2 class="name">${item.name}</h2>
-  //               <span class="visibility">${item.visibility}</span>
-  //             </div>
-  //             <p class="description">${item.description}</p>
-  //             <p class="topics">${item.topics[i]}</p>
-  //           </div>
-  //         </div>
-  //         `;
-  //       APIdata.innerHTML = html;
-  //       console.log(html);
-  //     }
-  //   } else {
-  //     APIdata.innerHTML = html;
-  //   }
-  // });
+  console.log(newData);
 
-  // data.map((item) => {
-  //   console.log(item.topics);
-
-  //   const html = `
-  //     <div class="APIitems">
-  //       <div class="APIitem">
-  //         <div class="title">
-  //           <h2 class="name">${item.name}</h2>
-  //           <span class="visibility">${item.visibility}</span>
-  //         </div>
-  //         <p class="description">${item.description}</p>
-  //         <p class="topics"></p>
-  //       </div>
-  //     </div>
-  //     `;
-  //   APIdata.innerHTML = html;
-
-  //   const topicsHTML = document.querySelector(".topics");
-
-  //   for (let i = 0; i < item.topics.length; i++) {
-  //     let htmlP = `<p class="topics">${item.topics[i]}</p>`;
-  //     console.log(item.topics[i]);
-  //     topicsHTML.innerHTML = htmlP;
-  //   }
-  // });
+  APIdata.insertAdjacentHTML("beforeend", newData);
 }
 
 // click more btn
 moreData.addEventListener("click", () => {
-  countPages += 5;
+  countPages += 1;
   ajax(
-    `https://api.github.com/orgs/facebook/repos?per_page=${countPages}&page=1`
-  ).then((data) => render(data));
+    `https://api.github.com/orgs/facebook/repos?per_page=5&page=${countPages}`
+  ).then((data) => ren(data));
 });
 
 ajax(`https://api.github.com/orgs/facebook/repos?per_page=5&page=1`).then(
